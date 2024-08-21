8f35028318ecd031718ac214b8c11c2d55ab4ac2
itsspyner <160397729+itsspyner@users.noreply.github.com>
2024-08-21 16:40:04 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Writable } = require('stream');
-const { resume } = require('./resumeGenerator');
+const { resume } = require('./resumeCreator');
 
 const app = express();
 app.use(bodyParser.json());
